2b3399b3acfdb98893e2f3fb7aaf49cfa6cb6d5e
yanJuicy <hanjub3@gmail.com>
2024-09-19 07:14:25 +0900

    회원가입에 Recruiter, JobSeeker 구분하는 로직 추가

--- src/main/java/com/luv2code/jobportal/entity/RecruiterProfile.java
+++ src/main/java/com/luv2code/jobportal/entity/RecruiterProfile.java
@@ -46,6 +46,10 @@ public class RecruiterProfile {
         this.profilePhoto = profilePhoto;
     }
 
+    public RecruiterProfile(Users users) {
+        this.userId = users;
+    }
+
     public int getUserAccountId() {
         return userAccountId;
     }
--- src/main/java/com/luv2code/jobportal/service/UsersService.java
+++ src/main/java/com/luv2code/jobportal/service/UsersService.java
@@ -1,6 +1,10 @@
 package com.luv2code.jobportal.service;
 
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+import com.luv2code.jobportal.entity.RecruiterProfile;
 import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
+import com.luv2code.jobportal.repository.RecruiterProfileRepository;
 import com.luv2code.jobportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +16,29 @@ import java.util.Optional;
 public class UsersService {
 
     private final UserRepository userRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
     @Autowired
-    public UsersService(UserRepository userRepository) {
+    public UsersService(UserRepository userRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
         this.userRepository = userRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return userRepository.save(users);
+        Users savedUser = userRepository.save(users);
+
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        if (userTypeId == 1) {
+            recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        } else {
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
+
+        return savedUser;
     }
 
     public Optional<Users> getUserByEmail(String email) {
