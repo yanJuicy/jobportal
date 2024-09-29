1861ffa0af7d3ba09e13088f6ca20fac2e011809
yanJuicy <hanjub3@gmail.com>
2024-09-29 23:48:23 +0900

    회원가입 로직에 비밀번호 암호화 추가

--- src/main/java/com/luv2code/jobportal/service/UsersService.java
+++ src/main/java/com/luv2code/jobportal/service/UsersService.java
@@ -7,6 +7,7 @@ import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
 import com.luv2code.jobportal.repository.RecruiterProfileRepository;
 import com.luv2code.jobportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,17 +19,20 @@ public class UsersService {
     private final UserRepository userRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UserRepository userRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
+    public UsersService(UserRepository userRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = userRepository.save(users);
 
         int userTypeId = users.getUserTypeId().getUserTypeId();
