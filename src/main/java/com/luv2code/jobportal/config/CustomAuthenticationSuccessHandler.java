15fb1cd8a79b4e1524f0d9883332648e25331c92
yanJuicy <hanjub3@gmail.com>
2024-10-04 04:18:38 +0900

    인증 성공 핸들러 작성

---
+++ src/main/java/com/luv2code/jobportal/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,31 @@
+package com.luv2code.jobportal.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        System.out.println("유저 " + username + " 로그인");
+        boolean hasJobSeekerRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("Job Seeker"));
+        boolean hasRecruiterRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("Recruiter"));
+
+        if (hasRecruiterRole || hasJobSeekerRole) {
+            response.sendRedirect("/dashboard");
+        }
+
+    }
+}
